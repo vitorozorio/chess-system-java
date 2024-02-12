61e23de1162bb525153b136b5cbf639832c91a06
VITOR OZORIO <vitormanoelbasilio357@gmail.com>
2024-02-12 18:39:08 -0300

    starting to implement Board and Piece

--- chess-system-java/src/main/java/org/nelio/xadres/program/Main.java
+++ chess-system-java/src/main/java/org/nelio/xadres/program/Main.java
@@ -1,11 +1,13 @@
 package org.nelio.xadres.program;
 
+import org.nelio.xadres.boardgame.Board;
 import org.nelio.xadres.boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
         Position position = new Position(1 ,2);
+        Board board = new Board(8,8);
 
         System.out.println(position);
 
