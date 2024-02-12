071f0932de8b74aa7fa61ef5e92795ebd71275e2
VITOR OZORIO <vitormanoelbasilio357@gmail.com>
2024-02-12 18:23:12 -0300

    first class: position

---
+++ chess-system-java/src/main/java/org/nelio/xadres/program/Main.java
@@ -0,0 +1,13 @@
+package org.nelio.xadres.program;
+
+import org.nelio.xadres.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position position = new Position(1 ,2);
+
+        System.out.println(position);
+
+    }
+}
